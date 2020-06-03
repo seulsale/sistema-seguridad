fee5d37418706950cf1384ab7e2a758065e08fe5
Sergio Saucedo <sergio.saucedo10@gmail.com>
2020-06-03 00:07:50 -0500

    Resuelve CORS policy

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from functools import wraps
+import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = '93jfaljef9ñ{a'
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+DEVICE_ACCESS_TOKEN = 'a53bb6163a6680eebfcc6c8aec76d011bec713c6'
 
 def login_required(f):
     @wraps(f)
@@ -29,6 +33,17 @@ def login():
             session['logged_in'] = True
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+@app.route('/api/motion')
+def sensors():
+    res = requests.get('http://10.0.0.3:8080/sensors.json?sense=motion_active')
+    res = res.json()
+    if len(res['motion_active']['data']) > 1:
+        print('MOVIMIENTO')
+        requests.post(f'https://api.particle.io/v1/devices/34002a000f47363336383437/trigger?access_token={DEVICE_ACCESS_TOKEN}', {
+            'params': 'on'
+        })
+    return res
 
 @app.route('/logout')
 def logout():
